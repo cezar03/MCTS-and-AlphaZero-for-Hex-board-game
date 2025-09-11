dcfba54d261630f34a9ab59a1948b70cc6744911
Noortje van Maldegem <n.vanmaldegem@student.maastrichtuniversity.nl>
2025-09-11 15:41:04 +0200

    Create Maven project

---
+++ src/main/java/nl/unimaas/dsai/team04/App.java
@@ -0,0 +1,13 @@
+package nl.unimaas.dsai.team04;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
