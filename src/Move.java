f4ba0ad79928d506159d34c8b5f2a8c00421c821
Lara Oriol Cabrera <l.oriolcabrera@student.maastrichtuniversity.nl>
2025-11-06 18:46:29 +0100

    Add 4 java files (selection) to my personal branch

---
+++ src/Move.java
@@ -0,0 +1,13 @@
+public class Move {
+    public String coordinate; // move coordinate
+
+    // constructor 
+    public Move(String coordinate){
+        this.coordinate = coordinate; 
+    }
+
+    // getter 
+    public String getCoordinate(){
+        return coordinate; 
+    }
+}
